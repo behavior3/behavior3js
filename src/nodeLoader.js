365a47e5415c65265850b831bb2341e367ada9c0
Erik Benson <efbenson@gmail.com>
2016-08-13 22:15:23 -0400

    feat: implements node detection in a cleaner way

--- src/nodeLoader.js
+++ src/nodeLoader.js
@@ -1,9 +1,9 @@
-var b3 = {};
+// test for nodejs runtime http://stackoverflow.com/questions/4224606/how-to-check-whether-a-script-is-running-under-node-js
+if (typeof module !== 'undefined' && this.module !== module) {
+    var b3 = {};
 
-if (typeof module === 'undefined') {
-    var module = { exports: {}};
+    module.exports = {
+        b3: b3
+    };
 }
 
-module.exports = {
-    b3: b3
-};
